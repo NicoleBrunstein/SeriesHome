18e3b15f7459c895879c424b007ba628e24d9151
NICOLE BRUNSTEIN <47492985@ort>
2023-10-12 15:47:53 -0300

    12

--- Models/BD.cs
+++ Models/BD.cs
@@ -4,6 +4,11 @@ using Dapper;
 namespace serieshome.Models;
 
 public class BD
-{
-    private static string _connectionString = @"Server=localhost; DataBase=PreguntadOrt;Trusted_Connection=True;";
+{  private static string _connectionString = @"Server=localhost; DataBase=BDSeries;Trusted_Connection=True;";
+  
+
+    using(SqlConnection db = new SqlConnection(_connectionString)){
+        string sql = "SELECT Nombre, ImagenSerie FROM Series";
+        _ListadoSeries = db.Query<Series>(sql).ToList();
+    }
 }
--- Models/Series.cs
+++ Models/Series.cs
@@ -6,7 +6,7 @@ public class Series
 {
      public int IdSerie {get;set;}
      public string Nombre {get;set;}
-     public datetime Anoinicio {get;set;}
+     public int Anoinicio {get;set;}
      public string Sinopsis {get;set;}
      public string imgSerie {get;set;}
 }
